9c3012a64432b1979944122ba6d217d6e0c19591
Axelmanza <manzanares712@gmail.com>
2020-05-19 17:09:35 +0200

    professional

---
+++ main.ts
@@ -0,0 +1,8 @@
+import {Professional} from "./professional";
+
+let alex = new Professional('alex', 28, 'masc', 76, 183, 'brown', 'brown', 'european', true, 'spanish', 0, 'b-ball coach');
+let jiaen = new Professional('jiaen', 28, 'masc', 76, 186, 'black', 'brown', 'asiatic', false, 'spanish', 3, 'b-ball coach');
+
+console.log(alex.print());
+console.log(jiaen.print());
+
