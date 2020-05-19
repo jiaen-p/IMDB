7f60eef76b9102aa7ece4bb7dc0e05b4f8f73e5e
Axelmanza <manzanares712@gmail.com>
2020-05-19 17:26:30 +0200

    IMDB creado

---
+++ imdb.ts
@@ -0,0 +1,9 @@
+
+import {Movie} from "./Movie";
+
+export class Imdb{
+    public peliculas: Movie[];
+    constructor(peliculas: Movie[]){
+        this.peliculas = peliculas;
+    }
+}
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import {Professional} from "./professional";
+import {Imdb} from "./imdb";
 
 let alex = new Professional('alex', 28, 'masc', 76, 183, 'brown', 'brown', 'european', true, 'spanish', 0, 'b-ball coach');
 let jiaen = new Professional('jiaen', 28, 'masc', 76, 186, 'black', 'brown', 'asiatic', false, 'spanish', 3, 'b-ball coach');
@@ -15,3 +16,5 @@ m1.print()
 
 let m2 = new Movie("Guns Akimbo", 2020, "UK, New Zealand and West Germany", "Comedy and Action")
 m2.print()
+
+let myImdb = new Imdb([m1, m2]);
